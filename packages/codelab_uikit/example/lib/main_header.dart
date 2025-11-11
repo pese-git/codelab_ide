4c5e61b201d98e26c13c2efaaf59d27feb7bed52
Sergey Penkovsky <sergey.penkovsky@gmail.com>
2025-11-11 17:39:03 +0300

    feat: add hide/show for AIAssistantPanel with splitter, button in MainHeader

--- packages/codelab_uikit/example/lib/main_header.dart
+++ packages/codelab_uikit/example/lib/main_header.dart
@@ -3,10 +3,14 @@ import 'package:fluent_ui/fluent_ui.dart';
 class MainHeader extends StatelessWidget {
   final bool bottomPanelVisible;
   final VoidCallback onToggleBottomPanel;
+  final bool aiPanelVisible;
+  final VoidCallback onToggleAiPanel;
   const MainHeader({
     super.key,
     required this.bottomPanelVisible,
     required this.onToggleBottomPanel,
+    required this.aiPanelVisible,
+    required this.onToggleAiPanel,
   });
 
   @override
@@ -24,6 +28,17 @@ class MainHeader extends StatelessWidget {
         IconButton(icon: const Icon(FluentIcons.save), onPressed: () {}),
         IconButton(icon: const Icon(FluentIcons.play), onPressed: () {}),
         IconButton(icon: const Icon(FluentIcons.settings), onPressed: () {}),
+        IconButton(
+          icon: Icon(
+            semanticLabel: aiPanelVisible
+                ? 'Hide AI panel'
+                : 'Show AI panel',
+            aiPanelVisible
+                ? FluentIcons.comment_urgent
+                : FluentIcons.comment,
+          ),
+          onPressed: onToggleAiPanel,
+        ),
         IconButton(
           icon: Icon(
             semanticLabel: bottomPanelVisible
