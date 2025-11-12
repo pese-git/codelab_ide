083d0a9c7283a762acb049796f0297275ce452d4
Sergey Penkovsky <sergey.penkovsky@gmail.com>
2025-11-12 14:36:52 +0300

    feat: add customizable slots for BottomPanel tabs, allow easy terminal and output integration in showcase

--- packages/codelab_uikit/example/lib/main.dart
+++ packages/codelab_uikit/example/lib/main.dart
@@ -1,5 +1,12 @@
+import 'dart:convert';
+import 'dart:io';
+
 import 'package:fluent_ui/fluent_ui.dart';
 import 'package:codelab_uikit/codelab_uikit.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_pty/flutter_pty.dart';
+import 'package:xterm/xterm.dart';
 
 void main() => runApp(const MyApp());
 
@@ -171,7 +178,9 @@ class _IdeRootPageState extends State<IdeRootPage> {
                                       50,
                                       panelHeight - 100,
                                     ),
-                                    child: const BottomPanel(),
+                                    child: const BottomPanel(
+                                      terminalSlot: BottomTerminal(),
+                                    ),
                                   ),
                                 ],
                               )
@@ -221,5 +230,103 @@ class _IdeRootPageState extends State<IdeRootPage> {
         },
       ),
     );
+  }
+}
+
+class BottomTerminal extends StatefulWidget {
+  const BottomTerminal({super.key});
+
+  @override
+  State<StatefulWidget> createState() {
+    return BottomTerminalState();
+  }
+}
+
+class BottomTerminalState extends State<BottomTerminal> {
+  final terminal = Terminal(maxLines: 10000);
+
+  final terminalController = TerminalController();
+
+  late final Pty pty;
+
+  @override
+  void initState() {
+    super.initState();
+
+    WidgetsBinding.instance.endOfFrame.then((_) {
+      if (mounted) _startPty();
+    });
+  }
+
+  void _startPty() {
+    pty = Pty.start(
+      shell,
+      columns: terminal.viewWidth,
+      rows: terminal.viewHeight,
+    );
+
+    pty.output
+        .cast<List<int>>()
+        .transform(Utf8Decoder())
+        .listen(terminal.write);
+
+    pty.exitCode.then((code) {
+      terminal.write('the process exited with exit code $code');
+    });
+
+    terminal.onOutput = (data) {
+      pty.write(const Utf8Encoder().convert(data));
+    };
+
+    terminal.onResize = (w, h, pw, ph) {
+      pty.resize(h, w);
+    };
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: TerminalView(
+        terminal,
+        controller: terminalController,
+        autofocus: true,
+        backgroundOpacity: 0.7,
+        onSecondaryTapDown: (details, offset) async {
+          final selection = terminalController.selection;
+          if (selection != null) {
+            final text = terminal.buffer.getText(selection);
+            terminalController.clearSelection();
+            await Clipboard.setData(ClipboardData(text: text));
+          } else {
+            final data = await Clipboard.getData('text/plain');
+            final text = data?.text;
+            if (text != null) {
+              terminal.paste(text);
+            }
+          }
+        },
+      ),
+    );
+  }
+
+  bool get isDesktop {
+    if (kIsWeb) return false;
+    return [
+      TargetPlatform.windows,
+      TargetPlatform.linux,
+      TargetPlatform.macOS,
+    ].contains(defaultTargetPlatform);
+  }
+
+  String get shell {
+    if (Platform.isMacOS || Platform.isLinux) {
+      return Platform.environment['SHELL'] ?? 'bash';
+    }
+
+    if (Platform.isWindows) {
+      return 'cmd.exe';
+    }
+
+    return 'sh';
   }
 }
