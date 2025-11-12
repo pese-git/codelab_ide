f945aead72de5d49d82e09a7f4f3ce067edf6d8b
Sergey Penkovsky <sergey.penkovsky@gmail.com>
2025-11-12 15:19:59 +0300

    feat(MainPanelArea): add workspaceSlot and emptySlot for flexible content injection

--- packages/codelab_uikit/lib/widgets/panels/main_panel_area.dart
+++ packages/codelab_uikit/lib/widgets/panels/main_panel_area.dart
@@ -1,27 +1,23 @@
-import 'editor_panel.dart';
-import 'package:fluent_ui/fluent_ui.dart';
-import '../placeholder/start_wizard.dart';
+import 'package:flutter/widgets.dart';
 
 class MainPanelArea extends StatelessWidget {
-  final GlobalKey<EditorPanelState> editorPanelKey;
   final bool projectOpened;
-  final double editorSplitFraction;
-  final ValueChanged<double> onEditorDrag;
-  final void Function(String action) onWizardAction;
+  final Widget? workspaceSlot;
+  final Widget? emptySlot;
+
   const MainPanelArea({
     super.key,
-    required this.editorPanelKey,
     required this.projectOpened,
-    required this.editorSplitFraction,
-    required this.onEditorDrag,
-    required this.onWizardAction,
+    this.workspaceSlot,
+    this.emptySlot,
   });
 
   @override
   Widget build(BuildContext context) {
-    if (!projectOpened) {
-      return StartWizard(onAction: onWizardAction);
+    if (projectOpened) {
+      return workspaceSlot ?? const SizedBox.shrink();
+    } else {
+      return emptySlot ?? const SizedBox.shrink();
     }
-    return EditorPanel(key: editorPanelKey, label: 'Editor 1');
   }
 }
