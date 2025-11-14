d03925a780fc1838415fca2370b529f04052a5f7
Sergey Penkovsky <sergey.penkovsky@gmail.com>
2025-11-14 07:26:14 +0300

    fix imports

--- packages/codelab_uikit/lib/widgets/panels/run_and_debug/call_stack_section.dart
+++ packages/codelab_uikit/lib/widgets/panels/run_and_debug/call_stack_section.dart
@@ -1,5 +1,6 @@
 import 'package:fluent_ui/fluent_ui.dart';
-import '../expander_with_flexible_height.dart';
+
+import 'expander_with_flexible_height.dart';
 
 class CallStackSection extends StatelessWidget {
   final bool expanded;
