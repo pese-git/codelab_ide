8ce50ece8713647685beb2cd34acb73147566bdc
Sergey Penkovsky <penkovsky_sa@st.by>
2025-12-20 08:51:08 +0300

    refactor(editor): migrate from flutter_code_editor to code_forge
    BREAKING CHANGE: replaced CodeField/CodeController (flutter_code_editor) with CodeForge/CodeForgeController (code_forge); highlight imports removed; language detection removed; text style and undo/redo preserved. FocusNode support dropped due to lack of API in code_forge. Consumers must update usage and dependencies accordingly.

--- macos/Flutter/ephemeral/flutter_export_environment.sh
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,12 +1,11 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/sergey/fvm/versions/3.35.1"
-export "FLUTTER_APPLICATION_PATH=/Users/sergey/Projects/OpenIdeaLab/codelab_ide"
+export "FLUTTER_ROOT=/Users/penkovsky_sa/fvm/versions/3.38.5"
+export "FLUTTER_APPLICATION_PATH=/Users/penkovsky_sa/Projects/OpenSource/codelab-workspace/codelab_ide"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
-export "FLUTTER_CLI_BUILD_MODE=debug"
 export "DART_OBFUSCATION=false"
 export "TRACK_WIDGET_CREATION=true"
 export "TREE_SHAKE_ICONS=false"
--- packages/codelab_uikit/lib/widgets/editor/editor_code_field.dart
+++ packages/codelab_uikit/lib/widgets/editor/editor_code_field.dart
@@ -1,26 +1,6 @@
 import 'package:fluent_ui/fluent_ui.dart';
 import 'package:flutter/material.dart' as material;
-import 'package:flutter_code_editor/flutter_code_editor.dart';
-import 'package:highlight/highlight_core.dart';
-import 'package:flutter_highlight/themes/github.dart';
-import 'package:highlight/languages/dart.dart';
-import 'package:highlight/languages/python.dart';
-import 'package:highlight/languages/javascript.dart';
-import 'package:highlight/languages/typescript.dart';
-import 'package:highlight/languages/java.dart';
-import 'package:highlight/languages/cpp.dart';
-import 'package:highlight/languages/cs.dart';
-import 'package:highlight/languages/go.dart';
-import 'package:highlight/languages/rust.dart';
-import 'package:highlight/languages/swift.dart';
-import 'package:highlight/languages/kotlin.dart';
-import 'package:highlight/languages/php.dart';
-import 'package:highlight/languages/ruby.dart';
-import 'package:highlight/languages/xml.dart';
-import 'package:highlight/languages/css.dart';
-import 'package:highlight/languages/yaml.dart';
-import 'package:highlight/languages/json.dart';
-import 'package:highlight/languages/markdown.dart';
+import 'package:code_forge/code_forge.dart';
 
 class EditorCodeField extends StatefulWidget {
   final String content;
@@ -39,32 +19,21 @@ class EditorCodeField extends StatefulWidget {
 }
 
 class _EditorCodeFieldState extends State<EditorCodeField> {
-  late CodeController _controller;
-  late FocusNode _focusNode;
+  late CodeForgeController _controller;
+  late UndoRedoController _undoController;
 
   @override
   void initState() {
     super.initState();
-    _focusNode = FocusNode();
-    _controller = CodeController(
-      text: widget.content,
-      language: _getLanguage(widget.filePath),
-    );
+    _controller = CodeForgeController()..text = widget.content;
+    _undoController = UndoRedoController();
     _controller.addListener(_handleChanged);
   }
 
   @override
   void didUpdateWidget(covariant EditorCodeField oldWidget) {
     super.didUpdateWidget(oldWidget);
-    if (widget.filePath != oldWidget.filePath) {
-      _controller.dispose();
-      _controller = CodeController(
-        text: widget.content,
-        language: _getLanguage(widget.filePath),
-      );
-      _controller.addListener(_handleChanged);
-      setState(() {});
-    } else if (widget.content != _controller.text) {
+    if (widget.content != _controller.text) {
       WidgetsBinding.instance.addPostFrameCallback((_) {
         _controller.text = widget.content;
       });
@@ -77,8 +46,8 @@ class _EditorCodeFieldState extends State<EditorCodeField> {
 
   @override
   void dispose() {
-    _focusNode.dispose();
     _controller.dispose();
+    _undoController.dispose();
     super.dispose();
   }
 
@@ -86,70 +55,14 @@ class _EditorCodeFieldState extends State<EditorCodeField> {
   Widget build(BuildContext context) {
     return material.Material(
       color: material.Colors.transparent,
-      child: CodeTheme(
-        data: CodeThemeData(styles: githubTheme),
-        child: CodeField(
-          controller: _controller,
-          focusNode: _focusNode,
-          textStyle: const material.TextStyle(
-            fontFamily: 'monospace',
-            fontSize: 14,
-          ),
-          expands: true,
-          padding: const EdgeInsets.all(16),
+      child: CodeForge(
+        controller: _controller,
+        undoController: _undoController,
+        textStyle: const material.TextStyle(
+          fontFamily: 'monospace',
+          fontSize: 14,
         ),
       ),
     );
-  }
-
-  Mode? _getLanguage(String filePath) {
-    final fileName = filePath.split('/').last;
-    final extension = fileName.split('.').last.toLowerCase();
-    switch (extension) {
-      case 'dart':
-        return dart;
-      case 'py':
-        return python;
-      case 'js':
-      case 'jsx':
-        return javascript;
-      case 'ts':
-      case 'tsx':
-        return typescript;
-      case 'java':
-        return java;
-      case 'cpp':
-      case 'cc':
-      case 'cxx':
-      case 'c':
-        return cpp;
-      case 'cs':
-        return cs;
-      case 'go':
-        return go;
-      case 'rs':
-        return rust;
-      case 'swift':
-        return swift;
-      case 'kt':
-        return kotlin;
-      case 'php':
-        return php;
-      case 'rb':
-        return ruby;
-      case 'html':
-        return xml;
-      case 'css':
-        return css;
-      case 'yaml':
-      case 'yml':
-        return yaml;
-      case 'json':
-        return json;
-      case 'md':
-        return markdown;
-      default:
-        return markdown;
-    }
   }
 }
